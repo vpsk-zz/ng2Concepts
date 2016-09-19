f932231df771617edad8082fbc122325efd2711a
virendra <virendra.ps@accenture.com>
2016-09-19 14:31:03 +0530

    module changes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ContactModule } from './contact/contact.module';
 
 import { AppComponent }  from './app.component';
 import { HighlightDirective }  from './highlight.directive';
 import { TitleComponent }  from './title.component';
 
+
 import { UserService } from './user.service';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent,HighlightDirective,TitleComponent ],
+  imports: [ BrowserModule,ContactModule],
+  declarations: [ AppComponent,HighlightDirective,TitleComponent],
   bootstrap: [ AppComponent ],
   providers: [ UserService ],
 })
